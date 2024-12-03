e8a0f1a1bc1af9e6bf16f0a75960c0525005c09f
traitimtrongvang7 <giapbuihoang567@gmail.com>
2024-12-03 14:32:48 +0700

    Your commit message

---
+++ Assets/Script/LogoutUI.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using Firebase.Auth;
+using UnityEngine.SceneManagement;
+
+public class LogoutUI : MonoBehaviour
+{
+    private FirebaseAuth auth;  // Firebase Auth instance
+
+    private void Start()
+    {
+        // Lấy instance FirebaseAuth
+        auth = FirebaseAuth.DefaultInstance;
+    }
+
+    // Hàm để đăng xuất
+    public void LogoutUser()
+    {
+        // Kiểm tra xem người dùng có đăng nhập hay không
+        if (auth.CurrentUser != null)
+        {
+
+            auth.SignOut();
+            Debug.Log("Người dùng đã đăng xuất.");
+
+
+            PlayerPrefs.DeleteKey("username");
+            PlayerPrefs.Save();
+
+
+            SceneManager.LoadScene("SampleScene");
+        }
+        else
+        {
+            Debug.LogWarning("Không có người dùng nào đang đăng nhập.");
+        }
+    }
+}
